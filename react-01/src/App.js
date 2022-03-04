0ddc90a5d7a8d8a26fe9bbb19ae48133cb9f723d
abhikatkar <katkarabhi91@gmail.com>
2022-03-04 12:13:16 +0530

    Done

---
+++ react-01/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
